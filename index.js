94a9e2eae397ded98b7e639e3b8773639dbbe628
zepol7 <zepol7@gmail.com>
2023-04-25 21:56:56 -0500

    Ver_1

--- index.js
+++ index.js
@@ -1,33 +1,7 @@
 // Import stylesheets
 import './style.css';
-
 // Write Javascript code!
 const appDiv = document.getElementById('app');
-
-d3.csv(
-  'https://raw.githubusercontent.com/mwaskom/seaborndata/master/titanic.csv',
-  function (error, data) {
-    if (error) {
-      console.log('Error al leer el fichero titanic.csv');
-    } else {
-      //Incorporación de gráficos
-      // .....
-    }
-  }
-);
-
-var visualization = d3plus
-  .viz()
-  .container('#viz1')
-  .data([
-    { year: 1991, name: 'alpha', value: 15 },
-    { year: 1992, name: 'alpha', value: 20 },
-  ])
-  .type('bar')
-  .id('name')
-  .x('year')
-  .y('value')
-  .draw();
 
 d3.json(
   'https://raw.githubusercontent.com/raul27868/07MBIG-Visualizacion-Actividades-Guiadas/master/columnas.json',
@@ -45,35 +19,55 @@ d3.json(
   }
 );
 
+d3.csv(
+  'https://raw.githubusercontent.com/mwaskom/seaborn-data/master/titanic.csv',
+  function (error, data) {
+    if (error) {
+      console.log('Error al leer el fichero titanic.csv');
+    } else {
+      var visualization = d3plus
+        .viz()
+        .container('#viz1')
+        .data([
+          { year: 1991, name: 'alpha', value: 15 },
+          { year: 1992, name: 'alpha', value: 20 },
+        ])
+        .type('bar')
+        .id('name')
+        .x('year')
+        .y('value')
+        .draw();
+    }
+  }
+);
 
 var data = [
-  {"year": 1991, "name":"alpha", "value": 15},
-  {"year": 1991, "name":"beta", "value": 10},
-  {"year": 1991, "name":"gamma", "value": 5},
-  {"year": 1991, "name":"delta", "value": 50},
-  {"year": 1992, "name":"alpha", "value": 20},
-  {"year": 1992, "name":"beta", "value": 10},
-  {"year": 1992, "name":"gamma", "value": 10},
-  {"year": 1992, "name":"delta", "value": 43},
-  {"year": 1993, "name":"alpha", "value": 30},
-  {"year": 1993, "name":"beta", "value": 40},
-  {"year": 1993, "name":"gamma", "value": 20},
-  {"year": 1993, "name":"delta", "value": 17},
-  {"year": 1994, "name":"alpha", "value": 60},
-  {"year": 1994, "name":"beta", "value": 60},
-  {"year": 1994, "name":"gamma", "value": 25},
-  {"year": 1994, "name":"delta", "value": 32}
-]
-
-
+  { year: 1991, name: 'alpha', value: 15 },
+  { year: 1991, name: 'beta', value: 10 },
+  { year: 1991, name: 'gamma', value: 5 },
+  { year: 1991, name: 'delta', value: 50 },
+  { year: 1992, name: 'alpha', value: 20 },
+  { year: 1992, name: 'beta', value: 10 },
+  { year: 1992, name: 'gamma', value: 10 },
+  { year: 1992, name: 'delta', value: 43 },
+  { year: 1993, name: 'alpha', value: 30 },
+  { year: 1993, name: 'beta', value: 40 },
+  { year: 1993, name: 'gamma', value: 20 },
+  { year: 1993, name: 'delta', value: 17 },
+  { year: 1994, name: 'alpha', value: 60 },
+  { year: 1994, name: 'beta', value: 60 },
+  { year: 1994, name: 'gamma', value: 25 },
+  { year: 1994, name: 'delta', value: 32 },
+];
 
 // Crear instancia de D3plus
-var visualization = d3plus.viz()
-  .container("#viz3") // Seleccionar contenedor
+var visualization = d3plus
+  .viz()
+  .container('#viz3') // Seleccionar contenedor
   .data(data) // Establecer datos
-  .type("box") // Establecer tipo de gráfico
-  .id("name") // Establecer columna para etiquetas
-  .x("year") // Configurar eje X
-  .y("value") // Configurar eje Y  
+  .type('box') // Establecer tipo de gráfico
+  .id('name') // Establecer columna para etiquetas
+  .x('year') // Configurar eje X
+  .y('value') // Configurar eje Y
   .legend(true) // Mostrar leyenda
   .draw(); // Dibujar gráfico
